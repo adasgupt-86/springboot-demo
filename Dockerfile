90f51a4e8eb0d3b8c0534f6fe999d17143a88390
Abhishek Dasgupta <abhishek.dasgupta11@gmail.com>
2026-07-25 23:42:26 +0530

    Initial Spring Boot Enterprise Demo

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+#########################################
+# Stage 1
+#########################################
+
+FROM maven:3.9.9-eclipse-temurin-21 AS builder
+
+WORKDIR /app
+
+COPY pom.xml .
+
+COPY src ./src
+
+RUN mvn clean package -DskipTests
+
+#########################################
+# Stage 2
+#########################################
+
+FROM eclipse-temurin:21-jre
+
+LABEL maintainer="Abhishek Dasgupta"
+
+WORKDIR /app
+
+COPY --from=builder /app/target/*.jar app.jar
+
+EXPOSE 8080
+
+ENTRYPOINT ["java","-jar","app.jar"]
---
+++ src/main/java/com/devops/demo/controller/HomeController.java
@@ -0,0 +1,43 @@
+package com.devops.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.SpringVersion;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.net.InetAddress;
+import java.time.LocalDateTime;
+
+@Controller
+public class HomeController {
+
+    @Value("${app.environment:LOCAL}")
+    private String environment;
+
+    @GetMapping("/")
+    public String home(Model model) throws Exception {
+
+        InetAddress inetAddress = InetAddress.getLocalHost();
+
+        model.addAttribute("appName", "Enterprise DevOps Demo");
+        model.addAttribute("version", "v1.0.0");
+        model.addAttribute("environment", environment);
+
+        model.addAttribute("hostname", inetAddress.getHostName());
+        model.addAttribute("ip", inetAddress.getHostAddress());
+
+        model.addAttribute("javaVersion",
+                System.getProperty("java.version"));
+
+        model.addAttribute("springVersion",
+                SpringVersion.getVersion());
+
+        model.addAttribute("time",
+                LocalDateTime.now());
+
+        model.addAttribute("status", "HEALTHY");
+
+        return "index";
+    }
+}
